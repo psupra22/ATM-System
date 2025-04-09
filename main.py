87adbe66751d7f3ba7c600fdb408bc32f9a8c101
Platon Supranovich <psupra521@gmail.com>
2025-04-09 09:06:59 -0400

    Separated ATM loop from main

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
+import re
 import sys
 import shutil
 from atm import ATM
 
 def main() -> None:
     atm: ATM = ATM("atm.db")  # Opens the database
+    atm_loop(atm)
+    atm.close()
 
+
+def clear_screen():
+    columns = shutil.get_terminal_size().columns
+    print("\n" * columns)  # Creates a blank space instead of using system calls
+
+
+def get_input(atm: ATM) -> int:
+    while True:
+        atm.print_menu()
+        try:
+            return int(input("Choose: ").strip())
+        except ValueError:
+            clear_screen()
+            print("Input must be a integer")
+
+
+def get_amount(prompt: str) -> tuple[str, str]:
+    amount: str = input(prompt).strip()
+    matches = re.search(r"^(?P<dollars>-?\d+)\.(?P<cents>\d{2})$", amount)
+    if not matches: 
+        raise ValueError("Invalid amount")
+    
+    return matches.group("dollars"), matches.group("cents")
+
+
+def atm_loop(atm: ATM) -> None:
     while True:
         try:
             clear_screen()
@@ -26,15 +55,16 @@ def main() -> None:
         try:
             match choice:
                 case 1:
-                    amount: float = round(float(input("Amount to withdraw: ").strip()), 2) 
-                    atm.withdraw(amount)
+                    amount: tuple[str, str] = get_amount("Amount to withdraw: ")
+                    atm.withdraw(amount[0], amount[1])
                 case 2:
-                    amount = round(float(input("Amount to deposit: ").strip()), 2) 
-                    atm.deposit(amount)
+                    amount = get_amount("Amount to deposit: ")
+                    atm.deposit(amount[0], amount[1])
                 case 3:
-                    atm.transfer(round(float(input("Transfer amount: ").strip()), 2))
+                    amount = get_amount("Transfer amount: ")
+                    atm.transfer(amount[0], amount[1])
                 case 4:
-                    print(f"Current balance: ${atm.balance:.2f}")
+                    print(f"Current balance: {atm.balance_string}")
                 case 5:
                     atm.change_pin(input("Enter your pin: ").strip())
                 case 0:
@@ -44,23 +74,6 @@ def main() -> None:
                     print("Invalid option")
         except ValueError as message:
             print(message)
-
-    atm.close()
-
-
-def clear_screen():
-    columns = shutil.get_terminal_size().columns
-    print("\n" * columns)  # Creates a blank space instead of using system calls
-
-
-def get_input(atm: ATM) -> int:
-    while True:
-        atm.print_menu()
-        try:
-            return int(input("Choose: "))
-        except ValueError:
-            clear_screen()
-            print("Input must be a integer")
 
 
 if __name__ == "__main__":
